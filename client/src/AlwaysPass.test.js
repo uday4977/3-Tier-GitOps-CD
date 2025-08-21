41e5aa77785f6059fda593296c05dd5aad0c2550
Aditya Jaiswal <32607172+jaiswaladi246@users.noreply.github.com>
2025-08-21 10:42:56 +0530

    Delete client directory

--- client/src/AlwaysPass.test.js
+++
@@ -1,5 +0,0 @@
-for (let i = 1; i <= 20; i++) {
-  test(`always passes ${i}`, () => {
-    expect(true).toBe(true);
-  });
-}
--- client/src/components/AnimatedBanner.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function AnimatedBanner({ message }) {
-  return <div className="animated-banner">{message}</div>;
-}
-
-export default AnimatedBanner;
